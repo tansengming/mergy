b7282ff08db24042a4a8642cefe81e44e0f3dffd
Tan, Seng Ming <tansengming@gmail.com>
2009-02-03 13:17:23 +0800

    now works without a list file, can feed from cmd line instead

--- mergy.rb
+++ mergy.rb
@@ -1,14 +1,24 @@
+require 'rubygems'
 require 'open-uri'
 require 'hpricot'
 
 class Mergy
-  def initialize(filename)
-    @urls = File.read(filename).split("\n")
-    @hpricots = @urls.map{|url| Hpricot(open(url))}
+  def initialize(paths)
+    @paths = paths
+    @hpricots = @paths.map{|url| Hpricot(open(url))}
   end
-
+  
+  def self.from_list(filename)
+    new(File.read(filename).split("\n"))
+  end
+  
+  def self.from_cmdline(files)
+    new(files.to_a)
+  end
+  
   def to_s
     @str = ''
+    @str << doc_type
     @str << '<html>'
     @str << '<head>'
     @str << @hpricots.map{|h| h.at('head').inner_html}.join
@@ -17,12 +27,23 @@ class Mergy
     @str << @hpricots.map{|h| h.at('body').inner_html}.join
     @str << '</body></html>' 
   end
+  
+  def doc_type
+    if doc = @hpricots.first and el = doc.children.select{|e| e.class == Hpricot::DocType }
+      el.to_s
+    else
+      '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
+    end
+  end
 end
 
 if ARGV.first == '-h'
   RDoc::usage()
 elsif ARGV.length == 2 and ARGV.first == '-i'
-  puts Mergy.new(ARGV[1])
+  puts Mergy.from_list(ARGV[1])
+elsif ARGV.length > 0 and ARGV.first != '-i'
+  puts Mergy.from_cmdline(ARGV)
 else
   raise ArgumentError, RDoc::usage('usage')
 end if File.basename(__FILE__) == $0
